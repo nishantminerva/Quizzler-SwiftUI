730b6dedde67a2547f679901b132de8013a2346d
Nishant <nishant@tensorbule.com>
2024-02-15 16:43:04 +0530

    Initial Commit

---
+++ Quizzler/QuizzlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzlerApp.swift
+//  Quizzler
+//
+//  Created by Nishant Minerva on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct QuizzlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
